c23b17f3590525bb2ae620093adc091efc81205d
Julius Adeyemo <juliusadeyemo07@gmail.com>
2025-06-24 04:48:27 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const KEY = "f84fc31d";
 //1bd0358a
 //http://www.omdbapi.com/?i=tt3896198&apikey=1bd0358a
 
-
 export default function App() {
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
@@ -277,7 +276,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
